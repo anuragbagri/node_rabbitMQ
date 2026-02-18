5efd8d943859066aba108aae09885fa9323098ca
anurag11 <anurag123bagri@gmail.com>
2026-02-18 12:52:49 +0530

    uncomment producer and consumer code in src

--- src/producer.js
+++ src/producer.js
@@ -1,51 +1,51 @@
-// import amqp from "amqplib";
-// import { config } from "./config.js";
+import amqp from "amqplib";
+import { config } from "./config.js";
 
-// /**
-//  * @description function for producers. sends data to the rabbitmq
-//  * @params none
-//  * @returns nothing
-//  */
-// (async function sendMail(){
-//     try {
-//         const connection = await amqp.connect("amqp://localhost");
-//         const channel = await connection.createChannel();  // producer runs on channel
-//         const exchange = config.exchange_name || " ";
-//         const queue = config.queue_name || " ";
-//         const routingKey = config.routingkey_name || " ";
-//         const message = {
-//             to : "anurag111bagri@gmail.com",
-//             from : "hari@gmail.com",
-//             subject : "mail from hr",
-//             body : "something from second message"
-//         }
+/**
+ * @description function for producers. sends data to the rabbitmq
+ * @params none
+ * @returns nothing
+ */
+(async function sendMail(){
+    try {
+        const connection = await amqp.connect("amqp://localhost");
+        const channel = await connection.createChannel();  // producer runs on channel
+        const exchange = config.exchange_name || " ";
+        const queue = config.queue_name || " ";
+        const routingKey = config.routingkey_name || " ";
+        const message = {
+            to : "anurag111bagri@gmail.com",
+            from : "hari@gmail.com",
+            subject : "mail from hr",
+            body : "something from second message"
+        }
 
-//         await channel.assertExchange(exchange , "direct" , {durable : false}); // create the exchange (type exchange => one of the type of exchange) and durable means data should not be persistant. 
+        await channel.assertExchange(exchange , "direct" , {durable : false}); // create the exchange (type exchange => one of the type of exchange) and durable means data should not be persistant. 
         
-//         // queue
-//         await channel.assertQueue(queue , {durable : false}); // not persistant
+        // queue
+        await channel.assertQueue(queue , {durable : false}); // not persistant
         
         
-//         await channel.bindQueue(queue , exchange , routingKey);  // bind queue with the exchange using .bind(queue, exchange, routingkey); 
+        await channel.bindQueue(queue , exchange , routingKey);  // bind queue with the exchange using .bind(queue, exchange, routingkey); 
 
-//         // data to queue 
-//         channel.publish(exchange , routingKey , Buffer.from(JSON.stringify(message))); // send data to queue using the routing key.we have to send data only in form of buffer and in form of strings. 
-//         console.log("mail data was sent --> ", message)
+        // data to queue 
+        channel.publish(exchange , routingKey , Buffer.from(JSON.stringify(message))); // send data to queue using the routing key.we have to send data only in form of buffer and in form of strings. 
+        console.log("mail data was sent --> ", message)
 
-//         setTimeout(() => {
-//             connection.close();
-//         }, 500);
-//     }
-//     catch(err){
-//         console.log(err);
-//     }
-// }
-// )();
+        setTimeout(() => {
+            connection.close();
+        }, 500);
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+)();
 
 
 // ### the above code is for the single exchange,single queue and single consumer. 
 
-// this if for practice ... 
+// this is for practice ... 
 
 // import amqp from "amqplib";
 // import { config } from "./config";
